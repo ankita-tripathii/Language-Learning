eb5231fe0acf5b5d7ed56d8ffccc6bb86d7fe4b4
ankita-tripathii <ankitatripathii011296@gmail.com>
2023-10-04 16:07:32 +0530

    details component added in a home component

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,6 +13,7 @@ export default function Home() {
          <Navbarexample />
          <FirstCarousel />
          <Languagelist />
+         <Details/>
          <FooterExample />
       </Stack>
     </>
